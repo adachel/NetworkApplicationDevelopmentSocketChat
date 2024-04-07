cc8a01996c214d30a20413674997c29662ff0fc2
Dmitry Arzhevitin <adachel74@gmail.com>
2024-04-07 18:53:58 +0500

    hw sem3 ok

--- SocketChat/Controllers/UserController.cs
+++ SocketChat/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using SocketChat.BLL.Logic;
 using SocketChat.Common.Entities;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SocketChat.Controllers
 {
