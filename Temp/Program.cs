f87656213397c8a9a6e8cf55ecf44f63e637ee59
Dmitry Arzhevitin <adachel74@gmail.com>
2024-04-08 14:20:14 +0500

    hw sem3 ok

---
+++ Temp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Temp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileName fileName = new FileName();
+            fileName.RunAsync();
+
+            Thread.Sleep(7000);
+
+        }
+    }
+}
